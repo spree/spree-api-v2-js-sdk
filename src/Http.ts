8f8e46cd40034359a72d5414f8a50303d1672bbe
igor_b <bezsmertnyi.igor@gmail.com>
2019-02-04 10:52:03 +0100

    Implement GET method

--- src/Http.ts
+++ src/Http.ts
@@ -1,4 +1,21 @@
-export default class Orbit {
+import Axios, { AxiosInstance } from 'axios'
+
+export default class Http {
+  public host: string;
+  public axios: AxiosInstance
+
   constructor() {
+    this.host = process.env.SPREE_HOST || 'http://localhost:3000/api/v2/storefront'
+
+    this.axios = Axios.create({
+      baseURL: this.host,
+      headers: {
+        'Content-Type': 'application/vnd.api+json'
+      }
+    })
+  }
+
+  async get(q, params = {}) {
+    return await this.axios.get(q, params)
   }
 }
--- src/endpoints/Products.ts
+++ src/endpoints/Products.ts
@@ -1,7 +1,10 @@
-export default class Products {
+import Http from '../Http'
+
+export default class Products extends Http {
   public async exec() {
     try {
-      
+      const res = await this.get('products.json')
+      return await res.data
     } catch(err) {
       console.error(err)
     }
