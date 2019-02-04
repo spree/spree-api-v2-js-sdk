0c17bcacaacae48942acad48579e7c7c8820acad
igor_b <bezsmertnyi.igor@gmail.com>
2019-02-04 15:15:41 +0100

    Removing `.json` format

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
 export const Routes = {
-  productsPath: () =>`products.json`,
-  productPath: (id: string) => `products/${id}.json`
+  productsPath: () =>`products`,
+  productPath: (id: string) => `products/${id}`
 }
