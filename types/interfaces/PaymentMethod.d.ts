e9773a6f47282a3ab9beacdae0a4a4d5b0ba68f8
tniezg <tomek.niezgoda@mail.com>
2019-03-14 16:12:05 +0100

    Update TS typings for checkout endpoints

---
+++ types/interfaces/PaymentMethod.d.ts
@@ -0,0 +1,16 @@
+import { JsonApiDocument, JsonApiListResponse } from './JsonApi';
+import { ResultResponse } from './ResultResponse';
+export interface PaymentMethodAttr extends JsonApiDocument {
+    type: string;
+    id: string;
+    attributes: {
+        type: string;
+        name: string;
+        description: string;
+    };
+}
+export interface IPaymentMethods extends JsonApiListResponse {
+    data: PaymentMethodAttr[];
+}
+export interface IPaymentMethodsResult extends ResultResponse<IPaymentMethods> {
+}
