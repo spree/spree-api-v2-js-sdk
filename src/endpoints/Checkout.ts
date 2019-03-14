95eef0ad055308fe4391aabada1edbdfb6fa8d05
Agnieszka Rudnicka <agnieszka.rudnicka@sparksolutions.co>
2019-03-14 13:28:09 +0100

    Add interface for payment and shipping methods Add return types to Checkout endpoints

--- src/endpoints/Checkout.ts
+++ src/endpoints/Checkout.ts
@@ -1,42 +1,43 @@
 import { GET, PATCH, POST } from '../constants'
 import Http from '../Http'
 import { AddStoreCredit, CheckoutClass, NestedAttributes } from '../interfaces/endpoints/CheckoutClass'
-import { JsonApiListResponse } from '../interfaces/JsonApi'
+import { IOrderResult } from '../interfaces/Order'
+import { IPaymentMethodsResult } from '../interfaces/PaymentMethod'
 import { IQuery } from '../interfaces/Query'
-import { ResultResponse } from '../interfaces/ResultResponse'
+import { IShippingMethodsResult } from '../interfaces/ShippingMethod'
 import { IToken } from '../interfaces/Token'
 import { Routes } from '../routes'
 
 export default class Checkout extends Http implements CheckoutClass {
-  public async orderNext(token: IToken, params: IQuery = {}) {
-    return await this.spreeResponse(PATCH, Routes.checkoutNextPath(), token, params)
+  public async orderNext(token: IToken, params: IQuery = {}): Promise<IOrderResult> {
+    return await this.spreeResponse(PATCH, Routes.checkoutNextPath(), token, params) as IOrderResult
   }
 
-  public async orderUpdate(token: IToken, params: NestedAttributes) {
-    return await this.spreeResponse(PATCH, Routes.checkoutPath(), token, params)
+  public async orderUpdate(token: IToken, params: NestedAttributes): Promise<IOrderResult> {
+    return await this.spreeResponse(PATCH, Routes.checkoutPath(), token, params) as IOrderResult
   }
 
-  public async advance(token: IToken, params: IQuery = {}) {
-    return await this.spreeResponse(PATCH, Routes.checkoutAdvancePath(), token, params)
+  public async advance(token: IToken, params: IQuery = {}): Promise<IOrderResult> {
+    return await this.spreeResponse(PATCH, Routes.checkoutAdvancePath(), token, params) as IOrderResult
   }
 
-  public async complete(token: IToken, params: IQuery = {}) {
-    return await this.spreeResponse(PATCH, Routes.checkoutCompletePath(), token, params)
+  public async complete(token: IToken, params: IQuery = {}): Promise<IOrderResult> {
+    return await this.spreeResponse(PATCH, Routes.checkoutCompletePath(), token, params) as IOrderResult
   }
 
-  public async addStoreCredits(token: IToken, params: AddStoreCredit) {
-    return await this.spreeResponse(POST, Routes.checkoutAddStoreCreditsPath(), token, params)
+  public async addStoreCredits(token: IToken, params: AddStoreCredit): Promise<IOrderResult>  {
+    return await this.spreeResponse(POST, Routes.checkoutAddStoreCreditsPath(), token, params) as IOrderResult
   }
 
-  public async removeStoreCredits(token: IToken, params: IQuery = {}) {
-    return await this.spreeResponse(POST, Routes.checkoutRemoveStoreCreditsPath(), token, params)
+  public async removeStoreCredits(token: IToken, params: IQuery = {}): Promise<IOrderResult> {
+    return await this.spreeResponse(POST, Routes.checkoutRemoveStoreCreditsPath(), token, params) as IOrderResult
   }
 
-  public async paymentMethods(token: IToken): Promise<ResultResponse<JsonApiListResponse>> {
-    return await this.spreeResponse(GET, Routes.checkoutPaymentMethodsPath(), token) as any
+  public async paymentMethods(token: IToken): Promise<IPaymentMethodsResult> {
+    return await this.spreeResponse(GET, Routes.checkoutPaymentMethodsPath(), token) as IPaymentMethodsResult
   }
 
-  public async shippingMethods(token: IToken) {
-    return await this.spreeResponse(GET, Routes.checkoutShippingMethodsPath(), token)
+  public async shippingMethods(token: IToken): Promise<IShippingMethodsResult> {
+    return await this.spreeResponse(GET, Routes.checkoutShippingMethodsPath(), token) as IShippingMethodsResult
   }
 }
