86ea6ef35e28e8d686594c9fcfe8f17e6cba29f2
tniezg <tomek.niezgoda@mail.com>
2019-04-01 15:27:25 +0200

    Export src/helpers/Result for external use

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import * as errors from './errors';
+import Result from './helpers/Result';
 import Instance from './Instance';
 export declare const makeClient: (config: any) => Instance;
-export { errors };
+export { errors, Result };
