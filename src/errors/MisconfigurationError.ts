4927ca79b37b805e6ab4b3177a6a1b08745e293c
tniezg <tomek.niezgoda@mail.com>
2019-07-22 01:45:24 +0200

    Fallback to ES2015 Error inheritance strategy

--- src/errors/MisconfigurationError.ts
+++ src/errors/MisconfigurationError.ts
@@ -3,7 +3,7 @@ import SpreeSDKError from './SpreeSDKError'
 export default class MisconfigurationError extends SpreeSDKError {
   constructor(message: string) {
     super(`Incorrect request setup: ${message}`)
-    Object.setPrototypeOf(this, new.target.prototype)
+    Object.setPrototypeOf(this, MisconfigurationError.prototype)
     this.name = 'MisconfigurationError'
   }
 }
--- src/errors/SpreeSDKError.ts
+++ src/errors/SpreeSDKError.ts
@@ -1,1 +1,6 @@
-export default class SpreeSDKError extends Error {}
+export default class SpreeSDKError extends Error {
+  constructor(name: string) {
+    super(name)
+    Object.setPrototypeOf(this, SpreeSDKError.prototype)
+  }
+}
