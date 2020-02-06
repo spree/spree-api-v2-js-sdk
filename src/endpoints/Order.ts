9c7c28c43dacce8e65dbd496a8fd73168a91744a
Tomek Niezgoda <tomasz.niezgoda@sparksolutions.co>
2020-02-06 14:28:17 +0100

    Send order token with order status (#117)
    * Include token when calling order status endpoint
    
    * Update order status readme to include token information

--- src/endpoints/Order.ts
+++ src/endpoints/Order.ts
@@ -2,10 +2,11 @@ import { GET } from '../constants'
 import Http from '../Http'
 import { IOrderResult } from '../interfaces/Order'
 import { IQuery } from '../interfaces/Query'
+import { IToken } from '../interfaces/Token'
 import { Routes } from '../routes'
 
 export default class Order extends Http {
-  public async status(orderNumber: string, params: IQuery = {}): Promise<IOrderResult> {
-    return await this.spreeResponse(GET, Routes.orderStatusPath(orderNumber), {}, params) as IOrderResult
+  public async status(token: IToken, orderNumber: string, params: IQuery = {}): Promise<IOrderResult> {
+    return await this.spreeResponse(GET, Routes.orderStatusPath(orderNumber), token, params) as IOrderResult
   }
 }
