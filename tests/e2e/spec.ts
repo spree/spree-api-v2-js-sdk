73aadd27b9e1d9bcffe886dd7e5b6b113d681192
tniezg <tomek.niezgoda@mail.com>
2020-02-18 13:30:19 +0100

    Test path to order completion

--- tests/e2e/spec.ts
+++ tests/e2e/spec.ts
@@ -1,51 +1,80 @@
-import { makeClient } from '@spree/storefront-api-v2-sdk'
+import { Client, makeClient } from '@spree/storefront-api-v2-sdk'
+import { RelationType } from '@spree/storefront-api-v2-sdk/types/interfaces/Relationships'
 
 describe('using SDK client in browser', function () {
-  before(function () {
+  beforeEach(function () {
     const client = makeClient({
       host: 'http://spree:3000'
     })
     cy.wrap(client).as('client')
+    cy.fixture('order-full-address').as('orderFullAddress')
   })
 
-  it('creates new cart', function () {
-    const { client } = this
+  it('completes guest order', function () {
+    const { orderFullAddress } = this
+    const client: Client = this.client
 
-    cy.wrap(null).then(async () => {
-      const cartCreateResponse = await client.cart.create()
+    cy
+      .wrap(null)
+      .then(() => client.cart.create())
+      .then((cartCreateResponse) => {
+        const { token: orderToken, number: orderNumber } = cartCreateResponse.success().data.attributes
 
-      const orderToken = cartCreateResponse.success().data.attributes.token
+        return cy.wrap(null)
+          .then(() => client.cart.addItem({ orderToken }, { variant_id: '1', quantity: 1 }))
+          .then(() => client.checkout.orderUpdate({ orderToken }, { order: orderFullAddress }))
+          .then(() => client.checkout.shippingMethods({ orderToken }))
+          .then((shippingResponse) => {
+            const firstShipment = shippingResponse.success().data[0]
 
-      const addItemResponse = await client.cart.addItem({ orderToken }, { variant_id: '1', quantity: 1 })
-
-      const order = await client.checkout.orderUpdate({ orderToken }, {
-        order: {
-          bill_address_attributes: {
-            address1: '7735 Old Georgetown Road',
-            city: 'Bethesda',
-            country_iso: 'US',
-            firstname: 'John',
-            lastname: 'Snow',
-            phone: '3014445002',
-            state_name: 'MD',
-            zipcode: '20814'
-          },
-          email: 'john@snow.org',
-          ship_address_attributes: {
-            address1: '7735 Old Georgetown Road',
-            city: 'Bethesda',
-            country_iso: 'US',
-            firstname: 'John',
-            lastname: 'Snow',
-            phone: '3014445002',
-            state_name: 'MD',
-            zipcode: '20814'
-          },
-          special_instructions: 'Please leave at door'
-        }
+            return client.checkout.orderUpdate({ orderToken }, {
+              order: {
+                payments_attributes: [
+                  {
+                    payment_method_id: 3 // "Check" payment method.
+                  }
+                ],
+                shipments_attributes: [
+                  {
+                    id: +firstShipment.id,
+                    selected_shipping_rate_id: (firstShipment.relationships.shipping_rates.data as RelationType[])[0].id
+                  }
+                ]
+              }
+            })
+          })
+          .then(() => client.checkout.complete({ orderToken }))
+          .then((orderCompleteResponse) => expect(orderCompleteResponse.isSuccess()).to.be.true)
+          .then(() => client.order.status({ orderToken }, orderNumber ))
+          .then((statusResponse) => expect(statusResponse.isSuccess()).to.be.true)
       })
+  })
 
-      order.success()
-    })
+  it('shows cart', function () {
+    const client: Client = this.client
+
+    cy
+      .wrap(null)
+      .then(() => client.cart.create())
+      .then((cartCreateResponse) => {
+        const orderToken = cartCreateResponse.success().data.attributes.token
+        return client.cart.show({ orderToken })
+      })
+      .then((cartShowResponse) => expect(cartShowResponse.isSuccess()).to.be.true)
+  })
+
+  it('lists payment methods', function () {
+    const client: Client = this.client
+
+    cy
+      .wrap(null)
+      .then(() => client.cart.create())
+      .then((cartCreateResponse) => {
+        const orderToken = cartCreateResponse.success().data.attributes.token
+
+        return cy.wrap(null)
+          .then(() => client.checkout.paymentMethods({ orderToken }))
+          .then((paymentMethodsResponse) => expect(paymentMethodsResponse.isSuccess()).to.be.true)
+      })
   })
 })
