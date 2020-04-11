8342c87aca34676f747d8dc072f3ec10ae327a2e
Tomasz Niezgoda <tomasz.niezgoda@mail.com>
2020-04-11 10:44:52 +0200

    Export endpoints

---
+++ src/endpoints/index.ts
@@ -0,0 +1,10 @@
+import Account from './Account'
+import Authentication from './Authentication'
+import Cart from './Cart'
+import Checkout from './Checkout'
+import Countries from './Countries'
+import Order from './Order'
+import Products from './Products'
+import Taxons from './Taxons'
+
+export { Account, Authentication, Cart, Checkout, Countries, Order, Products, Taxons }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Client, { IClientConfig } from './Client'
 import * as errors from './errors'
 import Result from './helpers/Result'
 import Http from './Http'
+import * as endpoints from './endpoints'
 
 const makeClient = (config: IClientConfig = {}) => new Client(config)
 
-export { Client, Http, Result, errors, makeClient }
+export { Client, Http, Result, errors, makeClient, endpoints }
