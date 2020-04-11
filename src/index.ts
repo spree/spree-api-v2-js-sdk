4465113bac95488568eb6151eb59969426f7c753
Tomasz Niezgoda <tomasz.niezgoda@mail.com>
2020-04-11 10:22:30 +0200

    Export routes module

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import Client, { IClientConfig } from './Client'
 import * as errors from './errors'
 import Result from './helpers/Result'
 import Http from './Http'
+import * as routes from './routes'
 import * as endpoints from './endpoints'
 
 const makeClient = (config: IClientConfig = {}) => new Client(config)
 
-export { Client, Http, Result, errors, makeClient, endpoints }
+export { Client, Http, Result, errors, makeClient, endpoints, routes }
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,7 @@ import Client, { IClientConfig } from './Client';
 import * as errors from './errors';
 import Result from './helpers/Result';
 import Http from './Http';
+import * as routes from './routes';
 import * as endpoints from './endpoints';
 declare const makeClient: (config?: IClientConfig) => Client;
-export { Client, Http, Result, errors, makeClient, endpoints };
+export { Client, Http, Result, errors, makeClient, endpoints, routes };
