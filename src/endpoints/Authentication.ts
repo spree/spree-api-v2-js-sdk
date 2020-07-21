0eb54396ea7726c46b57be34d9213be119367bee
Tomasz Niezgoda <tomasz.niezgoda@mail.com>
2020-07-21 14:21:58 +0200

    Fix token requests return type

--- src/endpoints/Authentication.ts
+++ src/endpoints/Authentication.ts
@@ -1,14 +1,14 @@
 import { authParams, refreshParams } from '../helpers/auth'
 import Http from '../Http'
 import { AuthTokenAttr, RefreshTokenAttr } from '../interfaces/Authentication'
-import { ITokenResult } from '../interfaces/Token'
+import { IOAuthTokenResult } from '../interfaces/Token'
 import { Routes } from '../routes'
 
 export default class Authentication extends Http {
-  public async getToken(params: AuthTokenAttr): Promise<ITokenResult> {
-    return await this.spreeResponse('post', Routes.oauthTokenPath(), {}, authParams(params)) as ITokenResult
+  public async getToken(params: AuthTokenAttr): Promise<IOAuthTokenResult> {
+    return await this.spreeResponse('post', Routes.oauthTokenPath(), {}, authParams(params)) as IOAuthTokenResult
   }
-  public async refreshToken(params: RefreshTokenAttr): Promise<ITokenResult> {
-    return await this.spreeResponse('post', Routes.oauthTokenPath(), {}, refreshParams(params)) as ITokenResult
+  public async refreshToken(params: RefreshTokenAttr): Promise<IOAuthTokenResult> {
+    return await this.spreeResponse('post', Routes.oauthTokenPath(), {}, refreshParams(params)) as IOAuthTokenResult
   }
 }
