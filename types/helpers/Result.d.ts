b23ba7570cd2776d5065eaf7866c6f5d5de57159
Agnieszka Rudnicka <dev@codegreen.it>
2020-11-10 14:21:57 +0100

    Bump dependencies because of security

--- types/helpers/Result.d.ts
+++ types/helpers/Result.d.ts
@@ -1,6 +1,6 @@
 import { Result } from '../interfaces/Result';
 declare const _default: {
     success: <F, S>(value: any) => Result<F, S>;
-    fail: <F, S>(value: any) => Result<F, S>;
+    fail: <F_1, S_1>(value: any) => Result<F_1, S_1>;
 };
 export default _default;
