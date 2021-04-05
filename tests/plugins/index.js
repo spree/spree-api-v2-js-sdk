72a2e9df8e5e7d85fede813c7f275463d01a25cf
tniezg <tomek.niezgoda@mail.com>
2021-04-05 16:56:14 +0200

    Switch to cypress-log-to-output and remove webpack-preprocessor

--- tests/plugins/index.js
+++ tests/plugins/index.js
@@ -1,12 +1,5 @@
-const wp = require('@cypress/webpack-preprocessor')
+const logToOutput = require('cypress-log-to-output')
 
-module.exports = (on) => {
-  const options = {
-    webpackOptions: require('../webpack.config')
-  }
-
-  on('file:preprocessor', wp(options))
-
-  // Source: https://github.com/cypress-io/cypress/issues/3199
-  on('task', { log(message) { console.log(message); return null } })
+module.exports = (on, _config) => {
+  logToOutput.install(on)
 }
