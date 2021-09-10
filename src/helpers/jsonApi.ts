2279ba5c7ac33cc1caabe5887c0d44b1bd207840
tniezg <contact@tomaszniezgoda.com>
2021-09-10 17:31:41 +0200

    Add JSON:API helpers

---
+++ src/helpers/jsonApi.ts
@@ -0,0 +1,47 @@
+import type { JsonApiDocument, JsonApiResponse } from '../interfaces/JsonApi'
+import type { RelationType } from '../interfaces/Relationships'
+import { SpreeSDKError } from '../errors'
+
+const findDocument = <DocumentType extends JsonApiDocument>(
+  spreeSuccessResponse: JsonApiResponse,
+  relationType: RelationType
+): DocumentType | null => {
+  if (!spreeSuccessResponse.included) {
+    return null
+  }
+
+  return (
+    (spreeSuccessResponse.included.find(
+      (includedObject) => includedObject.type === relationType.type && includedObject.id === relationType.id
+    ) as DocumentType) || null
+  )
+}
+
+const findRelationshipDocuments = <DocumentType extends JsonApiDocument>(
+  spreeSuccessResponse: JsonApiResponse,
+  sourceDocument: JsonApiDocument,
+  relationshipName: string
+): DocumentType[] => {
+  if (!spreeSuccessResponse.included) {
+    return []
+  }
+
+  const oneOrManyDocumentReferences = sourceDocument.relationships[relationshipName]?.data
+  let documentReferences: RelationType[]
+
+  if (!documentReferences) {
+    throw new SpreeSDKError(`Incorrect relationship ${relationshipName}.`)
+  }
+
+  if (Array.isArray(oneOrManyDocumentReferences)) {
+    documentReferences = oneOrManyDocumentReferences
+  } else {
+    documentReferences = [oneOrManyDocumentReferences]
+  }
+
+  return documentReferences
+    .map<DocumentType>((typeObject: JsonApiDocument) => findDocument(spreeSuccessResponse, typeObject))
+    .filter(Boolean)
+}
+
+export { findDocument, findRelationshipDocuments }
