caca944a71e53cce6603a5ff89331f303447cfa8
Robert Nowakowski <aplegatt@gmail.com>
2021-09-23 16:14:39 +0200

    [NX-19] Adds image_transformaion suggesting in products endpoints

--- types/endpoints/Products.d.ts
+++ types/endpoints/Products.d.ts
@@ -1,8 +1,8 @@
 import Http from '../Http';
 import type { IProductResult, IProductsResult } from '../interfaces/Product';
-import type { IQuery } from '../interfaces/Query';
+import type { IProductsQuery } from '../interfaces/Query';
 import type { IToken } from '../interfaces/Token';
 export default class Products extends Http {
-    list(token?: IToken, params?: IQuery): Promise<IProductsResult>;
-    show(id: string, token?: IToken, params?: IQuery): Promise<IProductResult>;
+    list(token?: IToken, params?: IProductsQuery): Promise<IProductsResult>;
+    show(id: string, token?: IToken, params?: IProductsQuery): Promise<IProductResult>;
 }
--- types/interfaces/Query.d.ts
+++ types/interfaces/Query.d.ts
@@ -11,3 +11,9 @@ export interface IQuery {
     sort?: string;
     [customSpreeExtensionKey: string]: any;
 }
+export interface IProductsQuery extends IQuery {
+    image_transformation?: {
+        size?: string;
+        quality?: number;
+    };
+}
