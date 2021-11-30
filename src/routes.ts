029c09037a056e81f827c4a3f4702da210c9f592
tniezg <contact@tomaszniezgoda.com>
2021-11-30 15:12:38 +0100

    Encode every custom route parameter

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,19 @@ export const storefrontPath = `api/v2/storefront`
 
 const endpoints = {
   productsPath: (): string => `${storefrontPath}/products`,
-  productPath: (id: string): string => `${storefrontPath}/products/${id}`,
+  productPath: (id: string): string => `${storefrontPath}/products/${encodeURIComponent(id)}`,
   taxonsPath: (): string => `${storefrontPath}/taxons`,
-  taxonPath: (id: string): string => `${storefrontPath}/taxons/${encodeURI(id)}`,
+  taxonPath: (id: string): string => `${storefrontPath}/taxons/${encodeURIComponent(id)}`,
   countriesPath: (): string => `${storefrontPath}/countries`,
-  countryPath: (iso: string): string => `${storefrontPath}/countries/${iso}`,
+  countryPath: (iso: string): string => `${storefrontPath}/countries/${encodeURIComponent(iso)}`,
   cartPath: (): string => `${storefrontPath}/cart`,
   cartAddItemPath: (): string => `${storefrontPath}/cart/add_item`,
-  cartRemoveItemPath: (id: string): string => `${storefrontPath}/cart/remove_line_item/${id}`,
+  cartRemoveItemPath: (id: string): string => `${storefrontPath}/cart/remove_line_item/${encodeURIComponent(id)}`,
   cartEmptyPath: (): string => `${storefrontPath}/cart/empty`,
   cartSetItemQuantity: (): string => `${storefrontPath}/cart/set_quantity`,
   cartApplyCodePath: (): string => `${storefrontPath}/cart/apply_coupon_code`,
-  cartRemoveCodePath: (code?: string): string => `${storefrontPath}/cart/remove_coupon_code/${code}`,
+  cartRemoveCodePath: (code?: string): string =>
+    `${storefrontPath}/cart/remove_coupon_code/${encodeURIComponent(code)}`,
   cartRemoveAllCoupons: (): string => `${storefrontPath}/cart/remove_coupon_code`,
   /**
    * @deprecated Use {@link cartEstimateShippingRatesPath} instead.
@@ -39,33 +40,37 @@ const endpoints = {
   oauthTokenPath: (): string => `spree_oauth/token`,
   oauthRevokePath: (): string => `spree_oauth/revoke`,
   accountPath: (): string => `${storefrontPath}/account`,
-  accountAddressPath: (id: string): string => `${storefrontPath}/account/addresses/${id}`,
+  accountAddressPath: (id: string): string => `${storefrontPath}/account/addresses/${encodeURIComponent(id)}`,
   accountAddressesPath: (): string => `${storefrontPath}/account/addresses`,
-  accountAddressRemovePath: (id: string): string => `${storefrontPath}/account/addresses/${id}`,
+  accountAddressRemovePath: (id: string): string => `${storefrontPath}/account/addresses/${encodeURIComponent(id)}`,
   accountConfirmPath: (confirmationToken: string): string =>
-    `${storefrontPath}/account_confirmations/${confirmationToken}`,
+    `${storefrontPath}/account_confirmations/${encodeURIComponent(confirmationToken)}`,
   accountCreditCardsPath: (): string => `${storefrontPath}/account/credit_cards`,
   accountDefaultCreditCardPath: (): string => `${storefrontPath}/account/credit_cards/default`,
-  accountCreditCardRemovePath: (id: string): string => `${storefrontPath}/account/credit_cards/${id}`,
+  accountCreditCardRemovePath: (id: string): string =>
+    `${storefrontPath}/account/credit_cards/${encodeURIComponent(id)}`,
   accountCompletedOrdersPath: (): string => `${storefrontPath}/account/orders`,
-  accountCompletedOrderPath: (orderNumber: string): string => `${storefrontPath}/account/orders/${orderNumber}`,
+  accountCompletedOrderPath: (orderNumber: string): string =>
+    `${storefrontPath}/account/orders/${encodeURIComponent(orderNumber)}`,
   forgotPasswordPath: (): string => `${storefrontPath}/passwords`,
-  resetPasswordPath: (resetPasswordToken: string): string => `${storefrontPath}/passwords/${resetPasswordToken}`,
-  orderStatusPath: (orderNumber: string): string => `${storefrontPath}/order_status/${orderNumber}`,
+  resetPasswordPath: (resetPasswordToken: string): string =>
+    `${storefrontPath}/passwords/${encodeURIComponent(resetPasswordToken)}`,
+  orderStatusPath: (orderNumber: string): string => `${storefrontPath}/order_status/${encodeURIComponent(orderNumber)}`,
   pagesPath: (): string => `${storefrontPath}/cms_pages`,
-  pagePath: (id: string): string => `${storefrontPath}/cms_pages/${id}`,
+  pagePath: (id: string): string => `${storefrontPath}/cms_pages/${encodeURIComponent(id)}`,
   defaultCountryPath: (): string => `${storefrontPath}/countries/default`,
-  digitalAssetsDownloadPath: (token: string): string => `${storefrontPath}/digitals/${token}`,
+  digitalAssetsDownloadPath: (token: string): string => `${storefrontPath}/digitals/${encodeURIComponent(token)}`,
   menusPath: (): string => `${storefrontPath}/menus`,
-  menuPath: (id: string): string => `${storefrontPath}/menus/${id}`,
+  menuPath: (id: string): string => `${storefrontPath}/menus/${encodeURIComponent(id)}`,
   wishlistsPath: (): string => `${storefrontPath}/wishlists`,
-  wishlistPath: (token: string): string => `${storefrontPath}/wishlists/${token}`,
+  wishlistPath: (token: string): string => `${storefrontPath}/wishlists/${encodeURIComponent(token)}`,
   defaultWishlistPath: (): string => `${storefrontPath}/wishlists/default`,
-  wishlistsAddWishedItemPath: (token: string): string => `${storefrontPath}/wishlists/${token}/add_item`,
+  wishlistsAddWishedItemPath: (token: string): string =>
+    `${storefrontPath}/wishlists/${encodeURIComponent(token)}/add_item`,
   wishlistsUpdateWishedItemQuantityPath: (token: string, id: string): string =>
-    `${storefrontPath}/wishlists/${token}/set_item_quantity/${id}`,
+    `${storefrontPath}/wishlists/${encodeURIComponent(token)}/set_item_quantity/${encodeURIComponent(id)}`,
   wishlistsRemoveWishedItemPath: (token: string, id: string): string =>
-    `${storefrontPath}/wishlists/${token}/remove_item/${id}`
+    `${storefrontPath}/wishlists/${encodeURIComponent(token)}/remove_item/${encodeURIComponent(id)}`
 }
 
 export default endpoints
