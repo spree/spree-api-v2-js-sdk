9952cc34d06ec8f5454e78cb3ed7efdb73c30f85
tniezg <contact@tomaszniezgoda.com>
2021-12-01 15:21:54 +0100

    Replace wait-for-it with a more robust implementation

--- docker/express/express-docker-entrypoint.sh
+++ docker/express/express-docker-entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-/sdk/wait-for-it.sh -s docker-host:3000
+/sdk/wait-for-it.sh http://docker-host:3000/api/v2/storefront/products?per_page=1 -t 300
 
 node /app/index.js
--- tests/cypress-docker-entrypoint.sh
+++ tests/cypress-docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-/sdk/wait-for-it.sh -s express:5000
-/sdk/wait-for-it.sh -s docker-host:3000
+/sdk/wait-for-it.sh http://express:5000/ping -t 300
+/sdk/wait-for-it.sh http://docker-host:3000/api/v2/storefront/products?per_page=1 -t 300
 
 cypress run --browser chrome:stable --project /app
