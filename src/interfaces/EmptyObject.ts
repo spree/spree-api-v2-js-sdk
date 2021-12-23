90de691cf9a31c792ca81e806d3a9433f7995191
tniezg <contact@tomaszniezgoda.com>
2021-12-23 13:28:26 +0100

    Fix EmptyObjectResponse type

--- src/interfaces/EmptyObject.ts
+++ src/interfaces/EmptyObject.ts
@@ -1,5 +1,5 @@
 import { ResultResponse } from './ResultResponse'
 
-export type EmptyObjectResponse = {}
+export type EmptyObjectResponse = Record<string, never>
 
 export interface EmptyObjectResult extends ResultResponse<EmptyObjectResponse> {}
