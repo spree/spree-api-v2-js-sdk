ff6bd69ee9e9143df91dacf266f063636e89c23d
tniezg <contact@tomaszniezgoda.com>
2021-12-24 13:19:04 +0100

    Squash download digital asset method arguments

--- src/endpoints/DigitalAssets.ts
+++ src/endpoints/DigitalAssets.ts
@@ -2,10 +2,22 @@ import Http from '../Http'
 import routes from '../routes'
 import type { IQuery } from '../interfaces/Query'
 import type { IToken } from '../interfaces/Token'
-import type { DigitalAsset, DigitalAssetResult } from '../interfaces/DigitalAsset'
+import type { DigitalAsset, DigitalAssetResult, DownloadOptions } from '../interfaces/DigitalAsset'
+import squashAndPreparePositionalArguments from '../helpers/squashAndPreparePositionalArguments'
 
 export default class DigitalAssets extends Http {
-  public async download(token: IToken, assetToken: string, params: IQuery = {}): Promise<DigitalAssetResult> {
+  public async download(options: DownloadOptions): Promise<DigitalAssetResult>
+  /**
+   * @deprecated Use the combined options signature instead.
+   */
+  public async download(token: IToken, assetToken: string, params?: IQuery): Promise<DigitalAssetResult>
+  public async download(...allArguments: any[]): Promise<DigitalAssetResult> {
+    const [tokenOrOptions, positionalAssetToken, positionalParams = {}] = allArguments
+    const { assetToken, token, params } = squashAndPreparePositionalArguments(
+      [tokenOrOptions, { assetToken: positionalAssetToken }, positionalParams],
+      ['assetToken']
+    )
+
     return await this.spreeResponse<DigitalAsset>(
       'get',
       routes.digitalAssetsDownloadPath(assetToken),
