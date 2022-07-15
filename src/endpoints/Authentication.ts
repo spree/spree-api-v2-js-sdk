15d993edd9cd4a15f98783c4e5d6505ac2c0348a
Robert Nowakowski <aplegatt@gmail.com>
2022-07-15 09:36:38 +0200

    Authentication methods update

--- src/endpoints/Authentication.ts
+++ src/endpoints/Authentication.ts
@@ -11,57 +11,32 @@ import type {
 import type { IOAuthToken, IOAuthTokenResult } from '../interfaces/Token'
 import type { EmptyObjectResponse, EmptyObjectResult } from '../interfaces/EmptyObject'
 import routes from '../routes'
-import squashAndPreparePositionalArguments from '../helpers/squashAndPreparePositionalArguments'
 
 export default class Authentication extends Http {
-  public async getToken(options: GetTokenOptions): Promise<IOAuthTokenResult>
-  /**
-   * @deprecated Use the combined options signature instead.
-   */
-  public async getToken(params: AuthTokenAttr): Promise<IOAuthTokenResult>
-  public async getToken(...allArguments: any[]): Promise<IOAuthTokenResult> {
-    const [paramsOrOptions] = allArguments
-    const { token, params } = squashAndPreparePositionalArguments([paramsOrOptions], [])
-
+  public async getToken(options: GetTokenOptions): Promise<IOAuthTokenResult> {
     return await this.spreeResponse<IOAuthToken>(
       'post',
       routes.oauthTokenPath(),
-      token,
-      authParams(params as AuthTokenAttr)
+      {},
+      authParams(options as AuthTokenAttr)
     )
   }
 
-  public async refreshToken(options: RefreshTokenOptions): Promise<IOAuthTokenResult>
-  /**
-   * @deprecated Use the combined options signature instead.
-   */
-  public async refreshToken(params: RefreshTokenAttr): Promise<IOAuthTokenResult>
-  public async refreshToken(...allArguments: any[]): Promise<IOAuthTokenResult> {
-    const [paramsOrOptions] = allArguments
-    const { token, params } = squashAndPreparePositionalArguments([paramsOrOptions], [])
-
+  public async refreshToken(options: RefreshTokenOptions): Promise<IOAuthTokenResult> {
     return await this.spreeResponse<IOAuthToken>(
       'post',
       routes.oauthTokenPath(),
-      token,
-      refreshParams(params as RefreshTokenAttr)
+      {},
+      refreshParams(options as RefreshTokenAttr)
     )
   }
 
-  public async revokeToken(optons: RevokeTokenOptions): Promise<EmptyObjectResult>
-  /**
-   * @deprecated Use the combined options signature instead.
-   */
-  public async revokeToken(params: RevokeTokenAttr): Promise<EmptyObjectResult>
-  public async revokeToken(...allArguments: any[]): Promise<EmptyObjectResult> {
-    const [paramsOrOptions] = allArguments
-    const { token, params } = squashAndPreparePositionalArguments([paramsOrOptions], [])
-
+  public async revokeToken(options: RevokeTokenOptions): Promise<EmptyObjectResult> {
     return await this.spreeResponse<EmptyObjectResponse>(
       'post',
       routes.oauthRevokePath(),
-      token,
-      revokeParams(params as RevokeTokenAttr)
+      {},
+      revokeParams(options as RevokeTokenAttr)
     )
   }
 }
