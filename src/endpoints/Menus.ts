7334b89e2da81db143e536249ce11e5a56e8bd09
Robert Nowakowski <aplegatt@gmail.com>
2022-07-26 14:51:28 +0200

    Menus methods update

--- src/endpoints/Menus.ts
+++ src/endpoints/Menus.ts
@@ -13,30 +13,11 @@ import type { IQuery } from '../interfaces/Query'
 import routes from '../routes'
 
 export default class Menus extends Http {
-  public async list(options?: ListOptions): Promise<MenusResult>
-  /**
-   * @deprecated Use the combined options signature instead.
-   */
-  public async list(params?: MenusList): Promise<MenusResult>
-  public async list(...allArguments: any[]): Promise<MenusResult> {
-    const [paramsOrOptions = {}] = allArguments
-    const { token, params } = squashAndPreparePositionalArguments([paramsOrOptions], [])
-
-    return await this.spreeResponse<MenusResponse>('get', routes.menusPath(), token, params)
+  public async list(): Promise<MenusResult> {
+    return await this.spreeResponse<MenusResponse>('get', routes.menusPath(), {}, {})
   }
 
-  public async show(options: ShowOptions): Promise<MenuResult>
-  /**
-   * @deprecated Use the combined options signature instead.
-   */
-  public async show(id: string, params?: IQuery): Promise<MenuResult>
-  public async show(...allArguments: any[]): Promise<MenuResult> {
-    const [idOrOptions, positionalParams = {}] = allArguments
-    const { id, token, params } = squashAndPreparePositionalArguments(
-      [typeof idOrOptions === 'object' ? idOrOptions : { id: idOrOptions }, positionalParams],
-      ['id']
-    )
-
-    return await this.spreeResponse<Menu>('get', routes.menuPath(id), token, params)
+  public async show(options: ShowOptions): Promise<MenuResult> {
+    return await this.spreeResponse<Menu>('get', routes.menuPath(options.id), {}, {})
   }
 }
